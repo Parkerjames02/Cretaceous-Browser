117d54225c8d4bae26e03f2bfedbb60d15c7b742
Parkerjames02 <87055376+Parkerjames02@users.noreply.github.com>
2024-12-02 01:15:48 -0800

    Add files via upload

---
+++ FormMain.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Cretaceous_Browser__Official_
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.webBrowserDisplayPage = new System.Windows.Forms.WebBrowser();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageHomePage = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPageHomePage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Location = new System.Drawing.Point(16, 14);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(101, 46);
+            this.buttonGo.TabIndex = 0;
+            this.buttonGo.Text = "Scavenge";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxURL.Location = new System.Drawing.Point(136, 23);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(1706, 26);
+            this.textBoxURL.TabIndex = 1;
+            this.textBoxURL.TextChanged += new System.EventHandler(this.textBoxURL_TextChanged);
+            // 
+            // webBrowserDisplayPage
+            // 
+            this.webBrowserDisplayPage.Location = new System.Drawing.Point(6, 75);
+            this.webBrowserDisplayPage.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowserDisplayPage.Name = "webBrowserDisplayPage";
+            this.webBrowserDisplayPage.ScriptErrorsSuppressed = true;
+            this.webBrowserDisplayPage.Size = new System.Drawing.Size(1849, 863);
+            this.webBrowserDisplayPage.TabIndex = 2;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageHomePage);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(23, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.Padding = new System.Drawing.Point(3, 6);
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1869, 976);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPageHomePage
+            // 
+            this.tabPageHomePage.BackColor = System.Drawing.Color.Transparent;
+            this.tabPageHomePage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPageHomePage.Controls.Add(this.buttonGo);
+            this.tabPageHomePage.Controls.Add(this.webBrowserDisplayPage);
+            this.tabPageHomePage.Controls.Add(this.textBoxURL);
+            this.tabPageHomePage.Location = new System.Drawing.Point(4, 28);
+            this.tabPageHomePage.Name = "tabPageHomePage";
+            this.tabPageHomePage.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageHomePage.Size = new System.Drawing.Size(1861, 944);
+            this.tabPageHomePage.TabIndex = 0;
+            this.tabPageHomePage.Text = "Scavenge Tab";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage2.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPage2.Location = new System.Drawing.Point(4, 28);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1861, 944);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 28);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1861, 944);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Cretaceous_Browser__Official_.Properties.Resources.Cretaceous_Park;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.Text = "Cretaceous Browser";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPageHomePage.ResumeLayout(false);
+            this.tabPageHomePage.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.WebBrowser webBrowserDisplayPage;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageHomePage;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+    }
+}
---
+++ FormMain.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cretaceous_Browser__Official_
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            // Set my initial URL to start
+            textBoxURL.Text = "https://www.nhm.ac.uk/discover/dino-directory.html";
+            // Here we acutomatically go to it.
+            webBrowserDisplayPage.Navigate(textBoxURL.Text);
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// When the user clicks the Go button, this will run the code below to navigate to a webpage using the
+        /// URL in the textbox.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            // This will run the code below to naviagte to a web page using the
+            /// URL in the textbox
+            webBrowserDisplayPage.Navigate(textBoxURL.Text);
+            // So we don't have to manually intercept all errors use this
+            webBrowserDisplayPage.ScriptErrorsSuppressed = true;
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxURL_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
